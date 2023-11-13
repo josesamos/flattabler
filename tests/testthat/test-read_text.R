28e472622a7f4e5463438c9e7a8349f53944b6b1
josesamos <jsamos@ugr.es>
2023-11-13 18:44:30 +0100

    Include tests and download data in readme

--- tests/testthat/test-read_text.R
+++ tests/testthat/test-read_text.R
@@ -1,17 +1,17 @@
-# test_that("read_text_file()", {
-#   expect_equal({
-#     file <-
-#       system.file("extdata", "csvfolder/m4.csv", package = "flattabler")
-#     pt <- read_text_file(file)
-#     pt |> set_page(page = "M4")
-#   }, pt_ex |> set_page(page = "M4"))
-# })
-#
-# test_that("read_text_folder()", {
-#   expect_equal({
-#     folder <-
-#       system.file("extdata", "csvfolder", package = "flattabler")
-#     ptl <- read_text_folder(folder)
-#     ptl[[4]] |> set_page(page = "M4")
-#   }, pt_ex |> set_page(page = "M4"))
-# })
+test_that("read_text_file()", {
+  expect_equal({
+    file <-
+      system.file("extdata", "csvfolder/m4.csv", package = "flattabler")
+    pt <- read_text_file(file)
+    pt |> set_page(page = "M4")
+  }, pt_ex |> set_page(page = "M4"))
+})
+
+test_that("read_text_folder()", {
+  expect_equal({
+    folder <-
+      system.file("extdata", "csvfolder", package = "flattabler")
+    ptl <- read_text_folder(folder)
+    ptl[[4]] |> set_page(page = "M4")
+  }, pt_ex |> set_page(page = "M4"))
+})
